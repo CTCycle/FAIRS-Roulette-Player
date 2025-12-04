348dc1f84e9e7343ddf678d3d51bb3deadf409d4
CTCycle <thomas.virdis89@gmail.com>
2025-12-04 17:12:00 +0100

    changed constant name

--- FAIRS/app/utils/constants.py
+++ FAIRS/app/utils/constants.py
@@ -6,15 +6,15 @@ from os.path import abspath, join
 ###############################################################################
 ROOT_DIR = abspath(join(__file__, "../../../.."))
 PROJECT_DIR = join(ROOT_DIR, "FAIRS")
-RSC_PATH = join(PROJECT_DIR, "resources")
-DATA_PATH = join(RSC_PATH, "database")
+RESOURCES_PATH = join(PROJECT_DIR, "resources")
+DATA_PATH = join(RESOURCES_PATH, "database")
 SOURCE_PATH = join(DATA_PATH, "dataset")
 METADATA_PATH = join(DATA_PATH, "metadata")
 INFERENCE_PATH = join(DATA_PATH, "inference")
 EVALUATION_PATH = join(DATA_PATH, "validation")
-CHECKPOINT_PATH = join(RSC_PATH, "checkpoints")
-CONFIG_PATH = join(RSC_PATH, "configurations")
-LOGS_PATH = join(RSC_PATH, "logs")
+CHECKPOINT_PATH = join(RESOURCES_PATH, "checkpoints")
+CONFIG_PATH = join(RESOURCES_PATH, "configurations")
+LOGS_PATH = join(RESOURCES_PATH, "logs")
 PROCESS_METADATA_FILE = join(METADATA_PATH, "preprocessing_metadata.json")
 
 # [CONFIGURATIONS]
