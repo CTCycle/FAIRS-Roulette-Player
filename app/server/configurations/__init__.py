d4f69b3b6af5d3345d602d21b7405e1f8e43fd5b
Thomas Virdis <101833494+CTCycle@users.noreply.github.com>
2026-09-10 12:23:14 +0200

    refactor: narrow configuration package exports

--- app/server/configurations/__init__.py
+++ app/server/configurations/__init__.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
-from server.configurations.environment import ensure_environment_file, load_environment
+from server.configurations.environment import load_environment
 from server.configurations.management import ConfigurationManager
 from server.configurations.startup import (
     get_configuration_manager,
     get_server_settings,
     reload_settings_for_tests,
-    get_poll_interval_seconds,
 )
 from server.contracts.configuration import (
     DatabaseSettings,
@@ -21,12 +20,10 @@ from server.contracts.configuration import (
 
 __all__ = [
     "load_environment",
-    "ensure_environment_file",
     "ConfigurationManager",
     "get_configuration_manager",
     "get_server_settings",
     "reload_settings_for_tests",
-    "get_poll_interval_seconds",
     "DatabaseSettings",
     "JobsSettings",
     "DeviceSettings",
