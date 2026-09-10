f02e817bb8efb2efcde8277ced036e88677b3506
Thomas Virdis <101833494+CTCycle@users.noreply.github.com>
2026-09-10 09:40:21 +0200

    fix: remove obsolete inference padding import

--- app/server/learning/inference/player.py
+++ app/server/learning/inference/player.py
@@ -7,7 +7,7 @@ import pandas as pd
 from keras import Model
 from keras.utils import set_random_seed
 
-from server.common.constants import PAD_VALUE, STATES
+from server.common.constants import STATES
 from server.learning.betting.hold import StrategyHold
 from server.learning.betting.sizer import BetSizer
 from server.learning.betting.types import (
